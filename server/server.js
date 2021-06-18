21be32e81c097c7f18f0cc3874108a7ee09432b7
ChristopherPalacios <christopherr.palacios@gmail.com>
2021-06-18 14:47:23 -0400

    update folder structure and server.js for production

--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@ const passport = require("./middleware/authentication");
 const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 
-if (process.env.NODE_ENV === "production") {
-  //cd into client folder and npm run build when ready
-  app.use(express.static("client/build"));
-  app.use(cors());
-}
+// if (process.env.NODE_ENV === "production") {
+//   //cd into client folder and npm run build when ready
+//   app.use(express.static("client/build"));
+//   app.use(cors());
+// }
 
 //open routes
 const openRoutes = require("./routes/open");
@@ -27,6 +27,11 @@ app.use(cors());
 //Middleware to interact with body of requst
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
+}
+
 //Open routes
 app.use("/api/", openRoutes);
 
@@ -36,5 +41,14 @@ app.use("/api/*", passport.authenticate("jwt", { session: false }));
 app.use("/api/users", secureUserRoutes);
 app.use("/api/cart", secureCartRoutes);
 app.use("/api/order", secureOrderRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // Handle React routing, return all requests to React app
+  app.get("*", (request, response) => {
+    response.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
 
 app.listen(PORT, () => console.log(`Express is running on ${PORT}`));
