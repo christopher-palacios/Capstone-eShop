3cf4b87e5c7aafbf3a0f55aeb302add66b5e41ff
ChristopherPalacios <christopherr.palacios@gmail.com>
2021-06-18 17:24:03 -0400

    Update key

--- server/client/src/components/Stripe/Stripe.jsx
+++ server/client/src/components/Stripe/Stripe.jsx
@@ -4,7 +4,7 @@ import CheckoutModal from "../../components/CheckoutModal/CheckoutModal";
 import { AppContext } from "../../AppContext/AppContext";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
+const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_KEY}`);
 
 function Stripe() {
   const { showCheckoutModal, setShowCheckoutModal } = useContext(AppContext);
